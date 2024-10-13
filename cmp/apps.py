b163e0c3eae9d41a2fc1e4b33114db6bf1499ecd
David Morris <gm3dmo@github.com>
2024-10-13 13:47:47 +0100

    Adding a WAL writer for sqllite

--- cmp/apps.py
+++ cmp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CmpConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cmp"
+
+    def ready(self):
+        import cmp.db_signals
