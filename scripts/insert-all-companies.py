eb88745b2c754ea8e3551902870f69df066890df
David Morris <gm3dmo@github.com>
2024-11-28 15:42:15 +0000

    Adding new url to cope with private repo data store.

--- scripts/insert-all-companies.py
+++ scripts/insert-all-companies.py
@@ -1,19 +1,39 @@
 
 def run():
 
+    from pathlib import Path
+    import environ
+    import os
     import sys
     import urllib3
     import csv
     import time
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+    env = environ.Env(
+    DEBUG=(bool, False)
+    )
+
+    github_token= str(f"{env('READ_PAT')}")
+
+    print()
+    title = sys.argv[2]
+
+    headers = {
+       'Accept': 'application/vnd.github.v3.raw',
+       'Authorization': f'Bearer {github_token}'
+    }
+
     from cmp.models import Company
 
     print()
     title = sys.argv[2]
     
     start_fetch_time = time.time()
-    ref_data_url = "https://raw.githubusercontent.com/gm3dmo/old-cmp/main/data/company.csv"
+    ref_data_url = "https://api.github.com/repos/gm3dmo/old-cmp/contents/data/company.csv"
     http = urllib3.PoolManager()
-    r = http.request('GET', ref_data_url)
+    r = http.request('GET', ref_data_url, headers=headers)
     end_fetch_time = time.time()
     # load the response into a csv dictionary reader
     reader = csv.DictReader(r.data.decode('utf-8').splitlines())
--- scripts/insert-all-soldier-imprisonments.py
+++ scripts/insert-all-soldier-imprisonments.py
@@ -1,19 +1,37 @@
 
 def run():
 
+
+    from pathlib import Path
+    import environ
+    import os
     import sys
     import urllib3
     import csv
     import time
     from cmp.models import SoldierImprisonment
 
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+    env = environ.Env(
+    DEBUG=(bool, False)
+    )
+
+    github_token= str(f"{env('READ_PAT')}")
+
     print()
     title = sys.argv[2]
+
+    headers = {
+       'Accept': 'application/vnd.github.v3.raw',
+       'Authorization': f'Bearer {github_token}'
+    }
+
     
     start_fetch_time = time.time()
-    ref_data_url = "https://raw.githubusercontent.com/gm3dmo/old-cmp/main/data/soldier-imprisonment.csv"
+    ref_data_url = "https://api.github.com/repos/gm3dmo/old-cmp/contents/data/soldier-imprisonment.csv"
     http = urllib3.PoolManager()
-    r = http.request('GET', ref_data_url)
+    r = http.request('GET', ref_data_url, headers=headers)
     end_fetch_time = time.time()
     # load the response into a csv dictionary reader
     reader = csv.DictReader(r.data.decode('utf-8').splitlines())
